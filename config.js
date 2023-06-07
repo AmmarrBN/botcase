793d45141c8d65194ea43c5b4ab7bac31e3e17e2
AmmarXploit <104218064+AmmarrBN@users.noreply.github.com>
2023-06-07 08:07:47 +0700

    edit owner here

---
+++ config.js
@@ -0,0 +1,66 @@
+/*
+    Created & Base By ArxzyDev
+    Jual Sc? Neraka Paling Bawah
+    My Contact https://wa.me/6289513081052
+    
+   Notes:
+   Jika Ingin Recode Silakan Tapi Ingat Creditnya
+   Sc Adrian-MD Akan Terus Di Update.
+   Jika Ingin Beli Apikey Chat Owner Lolhuman
+   
+*/
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+global.apikey = '23e85b96e912dc1a0a7f24d7' // LOLHUMAN
+global.rosekey = '-' // ROSE
+
+//—————「 Set Nama Bot & Own 」—————//
+global.namabot = 'Kaaa - Botz'
+global.namaowner = 'AmmarBN'
+
+//—————「 Setting Owner 」—————//
+global.owner = ['6287708773367']
+global.nomerowner = '6287708773367'
+global.premium = ['6287708773367']
+
+//—————「 Set Wm 」—————//
+global.packname = 'Made By Kaaa - Botz\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n'
+global.author = 'AmmarBN'
+global.prefa = ['', '.','?','$','#']
+global.sp = '•'
+
+//—————「 Set Message 」—————//
+global.mess = {
+    success: '🤗Done, Oke Desu~',
+    admin: '❗Perintah Ini Hanya Bisa Digunakan Oleh Admin Group !',
+    botAdmin: '❗Perintah Ini Hanya Bisa Digunakan Ketika Bot Menjadi Admin Group !',
+    owner: '❗Perintah Ini Hanya Bisa Digunakan Oleh Owner !',
+    group: '❗Perintah Ini Hanya Bisa Digunakan Di Group Chat !',
+    private: '❗Perintah Ini Hanya Bisa Digunakan Di Private Chat !',
+    bot: '🤖 Fitur Khusus Pengguna Nomor Bot !',
+    wait: '⏳ Sedang Di Proses !',
+    endLimit: '🕊️ Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12 !',
+    error: '🚫 Fitur Sedang Error !',
+}
+
+//—————「 Set Limit 」—————//
+global.limitawal = {
+    premium: "Infinity",
+    free: 50
+}
+
+//—————「 Set Image 」—————//
+global.imageurl = 'https://i.ibb.co/4Zf3Mdv/20230526-084439.jpg'
+global.link = 'https://github.com/AmmarrBN'
+global.thumb = fs.readFileSync('./media/thumb.jpg')
+
+//—————「 Batas Akhir 」—————//
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+    fs.unwatchFile(file)
+    console.log(chalk.redBright(`Update'${__filename}'`))
+    delete require.cache[file]
+    require(file)
+})
